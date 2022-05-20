7287bd5d30b4923241e845715bca53abba665eb7
John Villas <joaovictor.vbt@gmail.com>
2022-05-19 21:29:03 -0300

    first commit

---
+++ dist/routes.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.routes = void 0;
+const express_1 = require("express");
+// import { UserController } from "./controllers/UserController"
+const routes = (0, express_1.Router)();
+exports.routes = routes;
---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import "reflect-metadata"
+import "dotenv/config"
+import express from "express"
+import { routes } from "./routes"
+
+const port = process.env.SERVER_PORT || 3003
+const app = express()
+
+app.use(express.json())
+// app.use(cors())
+app.use(routes)
+
+app.listen(port, () => {
+  console.log(`listenning at port ${port}`)
+})
